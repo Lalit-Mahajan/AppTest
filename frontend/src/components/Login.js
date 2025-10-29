7249a1f6eece02a8bd9b2affe511da1bb293db49
Lalit-Mahajan <139694592+Lalit-Mahajan@users.noreply.github.com>
2025-10-29 15:17:37 +0530

    frontend updated complete

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -10,7 +10,7 @@ function Login() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await axios.post(`${process.env.REACT_APP_API_URL}/login`, {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
                 email,
                 password,
             });
